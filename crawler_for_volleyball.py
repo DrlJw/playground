a285b9b28c25ad2c4054d33a229fdab2fbb02ad8
DrlJw <36078405+DrlJw@users.noreply.github.com>
2018-11-16 15:29:40 +0800

    Add files via upload

---
+++ crawler_for_volleyball.py
@@ -0,0 +1,16 @@
+import re
+import requests
+def crawler(url):
+    try:
+        r = requests.get(url)
+    except requests.exceptions.RequestException as err:
+        return err
+    r.encoding = r.apparent_encoding
+    pattern = re.compile('<figure class="team">\s+<a id=.*?Link1_.*?" href="/en/vnl/women/teams/.*?"><img id=.*?/></a>\s+<figcaption><a id=.*? href="/en/vnl/women/teams/.*?">(.*?)<\/a><\/figcaption>\s+</figure>\s+</td>\s+<td>(.*?)</td>\s+<td class="table-td-bold">(.*?)</td>\s+<td class="table-td-rightborder">(.*?)</td>', flags = re.M)
+    p = re.findall(pattern, r.text)
+    return p
+
+if __name__ == "__main__":
+    url = 'http://www.volleyball.world/en/vnl/women/results-and-ranking/round1'
+    result = crawler(url)
+    print(result)
